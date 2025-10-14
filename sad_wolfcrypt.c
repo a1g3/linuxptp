1f9617b50fa45e16c4e058b9b6de94f6a96d3a64
Alex Gebhard <dev@alge.email>
2025-10-14 10:06:12 -0500

    Add union to reduce memory usage

--- sad_wolfcrypt.c
+++ sad_wolfcrypt.c
@@ -20,12 +20,17 @@
 #define MAX_HEX_OUTPUT_LEN 1024
 #define MAX_KEY_LEN 1024
 
+enum MAC_TYPE { 
+    WC_HMAC, WC_CMAC, WC_ED25519
+};
+
 struct mac_data {
-    ed25519_key *ed25519_key;
-    Hmac *hmac;
-    Cmac *cmac;
-    int is_cmac;
-    int is_ed25519;
+    union {
+        ed25519_key *ed25519_key;
+        Hmac *hmac;
+        Cmac *cmac;
+    } wolfssl;
+    enum MAC_TYPE type;
     unsigned char key[MAX_KEY_LEN];
     int key_len;
 };
@@ -71,30 +76,30 @@ struct mac_data *sad_init_mac(integrity_alg_type algorithm,
     switch (algorithm) {
         case HMAC_SHA256_128:
         case HMAC_SHA256:
-            mac_data->hmac = calloc(1, sizeof(Hmac));
-            if (!mac_data->hmac) {
+            mac_data->wolfssl.hmac = calloc(1, sizeof(Hmac));
+            if (!mac_data->wolfssl.hmac) {
                 pr_err("Memory allocation for HMAC failed");
                 free(mac_data);
                 wolfCrypt_Cleanup();
                 return NULL;
             }
-            if (wc_HmacSetKey(mac_data->hmac, SHA256, key1, (word32)key1_len) != 0) {
+            if (wc_HmacSetKey(mac_data->wolfssl.hmac, SHA256, key1, (word32)key1_len) != 0) {
                 pr_err("Failed to set HMAC key");
                 sad_deinit_mac(mac_data);
                 return NULL;
             }
-            mac_data->is_cmac = 0;
+            mac_data->type = WC_HMAC;
             break;
 	    case CMAC_AES128:
         case CMAC_AES256:
-            mac_data->cmac = calloc(1, sizeof(Cmac));
-            if (!mac_data->cmac) {
+            mac_data->wolfssl.cmac = calloc(1, sizeof(Cmac));
+            if (!mac_data->wolfssl.cmac) {
                 pr_err("Memory allocation for CMAC failed");
                 free(mac_data);
                 wolfCrypt_Cleanup();
                 return NULL;
             }
-            if (wc_InitCmac(mac_data->cmac, key1, key1_len, 1, NULL) != 0) {
+            if (wc_InitCmac(mac_data->wolfssl.cmac, key1, key1_len, 1, NULL) != 0) {
                 pr_err("Failed to set CMAC key");
                 sad_deinit_mac(mac_data);
                 return NULL;
@@ -105,15 +110,15 @@ struct mac_data *sad_init_mac(integrity_alg_type algorithm,
             } else {
                 return NULL;
             }
-            mac_data->is_cmac = 1;
+            mac_data->type = WC_CMAC;
             mac_data->key_len = key1_len;
             break;
 
         case ED25519:
             int ret;
 
-            mac_data->ed25519_key = calloc(1, sizeof(ed25519_key));
-            if (!mac_data->ed25519_key) {
+            mac_data->wolfssl.ed25519_key = calloc(1, sizeof(ed25519_key));
+            if (!mac_data->wolfssl.ed25519_key) {
                 pr_err("Memory allocation for ED25519 key failed");
                 free(mac_data);
                 wolfCrypt_Cleanup();
@@ -125,8 +130,11 @@ struct mac_data *sad_init_mac(integrity_alg_type algorithm,
                 return NULL;
             }
 
-            wc_ed25519_init(mac_data->ed25519_key);
-            ret = wc_ed25519_import_private_key(key2, key2_len, key1, key2_len, mac_data->ed25519_key);
+            //byte priv[] = { 0x61, 0xF0, 0xFE, 0x64, 0x7C, 0xDA, 0xDD, 0x61, 0xB2, 0x24, 0x78, 0x57, 0x78, 0x07, 0x12, 0xAB, 0x69, 0xE7, 0xB5, 0x9D, 0x0B, 0xEE, 0x43, 0xF9, 0x23, 0x33, 0x4F, 0xE5, 0xC7, 0x55, 0x57, 0x0E };
+            //byte pub[]  = { 0x98, 0x18, 0x30, 0xA5, 0xF7, 0x70, 0xE5, 0xCD, 0x75, 0xE7, 0x3F, 0xC8, 0x92, 0xBF, 0x5A, 0xD3, 0x2B, 0xFA, 0x5F, 0xF2, 0x96, 0x7E, 0x9E, 0x26, 0x98, 0x54, 0x19, 0x27, 0xEC, 0x39, 0xBF, 0x93 };
+
+            wc_ed25519_init(mac_data->wolfssl.ed25519_key);
+            ret = wc_ed25519_import_private_key(key2, key2_len, key1, key2_len, mac_data->wolfssl.ed25519_key);
             if (ret != 0) {
                 pr_err("Failed to import ED25519 key");
                 sad_deinit_mac(mac_data);
@@ -134,7 +142,7 @@ struct mac_data *sad_init_mac(integrity_alg_type algorithm,
             }            
 
             mac_data->key_len = key1_len;
-            mac_data->is_ed25519 = 1;
+            mac_data->type = WC_ED25519;
             break;
         default:
             pr_err("Unknown integrity algorithm");
@@ -150,14 +158,19 @@ void sad_deinit_mac(struct mac_data *mac_data)
     if (!mac_data)
         return;
 
-    if (mac_data->hmac) {
-        free(mac_data->hmac);
-        mac_data->hmac = NULL;
+    if (mac_data->type == WC_HMAC && mac_data->wolfssl.hmac) {
+        free(mac_data->wolfssl.hmac);
+        mac_data->wolfssl.hmac = NULL;
     }
 
-    if (mac_data->cmac) {
-        free(mac_data->cmac);
-        mac_data->cmac = NULL;
+    if (mac_data->type == WC_CMAC && mac_data->wolfssl.cmac) {
+        free(mac_data->wolfssl.cmac);
+        mac_data->wolfssl.cmac = NULL;
+    }
+
+    if (mac_data->type == WC_ED25519 && mac_data->wolfssl.ed25519_key) {
+        free(mac_data->wolfssl.ed25519_key);
+        mac_data->wolfssl.ed25519_key = NULL;
     }
 
     free(mac_data);
@@ -173,37 +186,37 @@ int sad_hash(struct mac_data *mac_data,
         return 0;
     }
 
-    if (mac_data->is_cmac) {
+    if (mac_data->type == WC_CMAC) {
         if (mac_len > AES_BLOCK_SIZE || mac_len > MAX_DIGEST_LENGTH) {
             pr_err("mac_len exceeds CMAC size");
             return 0;
         }
 
 	    int ret = 0;
-        if ( (ret = wc_CmacUpdate(mac_data->cmac, data, (word32)data_len)) != 0) {
+        if ( (ret = wc_CmacUpdate(mac_data->wolfssl.cmac, data, (word32)data_len)) != 0) {
             
 	    pr_err("CMAC update failed with code 0x%08X", ret);
             return 0;
         }
 
-        if (wc_CmacFinal(mac_data->cmac, mac, (unsigned int *)&mac_len) != 0) {
+        if (wc_CmacFinal(mac_data->wolfssl.cmac, mac, (unsigned int *)&mac_len) != 0) {
             pr_err("CMAC finalization failed");
             return 0;
         }
 
-        if (wc_InitCmac(mac_data->cmac, mac_data->key, mac_data->key_len, WC_CMAC_AES, NULL) != 0) {
+        if (wc_InitCmac(mac_data->wolfssl.cmac, mac_data->key, mac_data->key_len, WC_CMAC_AES, NULL) != 0) {
             pr_err("CMAC initialization failed");
                 return 0;
         }
 
         return mac_len;
-    } else if (mac_data->is_ed25519) {
+    } else if (mac_data->type == WC_ED25519) {
         if (mac_len > ED25519_SIG_SIZE) {
             pr_err("mac_len exceeds ED25519 signature size");
             return 0;
         }
 
-        if (wc_ed25519_sign_msg(data, data_len, mac, (unsigned int *)&mac_len, mac_data->ed25519_key) != 0) {
+        if (wc_ed25519_sign_msg(data, data_len, mac, (unsigned int *)&mac_len, mac_data->wolfssl.ed25519_key) != 0) {
             pr_err("ED25519 signing failed");
             return 0;
         }
@@ -216,12 +229,12 @@ int sad_hash(struct mac_data *mac_data,
             return 0;
         }
 
-        if (wc_HmacUpdate(mac_data->hmac, data, (word32)data_len) != 0) {
+        if (wc_HmacUpdate(mac_data->wolfssl.hmac, data, (word32)data_len) != 0) {
             pr_err("HMAC update failed");
             return 0;
         }
 
-        if (wc_HmacFinal(mac_data->hmac, mac) != 0) {
+        if (wc_HmacFinal(mac_data->wolfssl.hmac, mac) != 0) {
             pr_err("HMAC finalization failed");
             return 0;
         }
@@ -242,7 +255,7 @@ int sad_verify(struct mac_data *mac_data,
         return -1;
     }
 
-    if (!mac_data->is_ed25519) {
+    if (mac_data->type != WC_ED25519) {
         if (!sad_hash(mac_data, data, data_len, digest_buf, mac_len)) {
             pr_err("Failed to compute hash for verification");
             return -1;
@@ -250,7 +263,7 @@ int sad_verify(struct mac_data *mac_data,
 
         return XMEMCMP(digest_buf, mac, mac_len);
     } else {
-        if (wc_ed25519_verify_msg(mac, mac_len, data, data_len, &ret, mac_data->ed25519_key) != 0) {
+        if (wc_ed25519_verify_msg(mac, mac_len, data, data_len, &ret, mac_data->wolfssl.ed25519_key) != 0) {
             pr_err("ED25519 verification failed");
             return -1;
         }
